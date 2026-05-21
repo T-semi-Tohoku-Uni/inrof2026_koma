12553a7626aa53d737339fc195bc7de5c1c8e164
shibatakeigo <sagankeigo17@gmail.com>
2026-05-21 17:22:47 +0900

    feat(komarm): add serial open

---
+++ src/komarm/src/feetech_joint.cpp
@@ -0,0 +1,79 @@
+#include <komarm/feetech_joint.hpp>
+
+koma::FeetechJointState::FeetechJointState(int id) {
+    this->id_ = id;
+}
+
+koma::Serial::Serial(const char* device_name) {
+    serial_fd_ = open_serial(device_name);
+}
+
+int koma::Serial::open_serial(const char* device_name) {
+    int fd = ::open(device_name, O_RDWR | O_NOCTTY | O_NONBLOCK);
+    if (fd < 0) {
+        RCLCPP_ERROR(rclcpp::get_logger("rclcpp"),
+                        "Serial Fail: could not open %s (%s)",
+                        device_name, std::strerror(errno));
+        return -1;
+    }
+    fcntl(fd, F_SETFL, 0);
+
+    struct termios tty;
+    if (tcgetattr(fd, &tty) != 0) {
+        RCLCPP_ERROR(rclcpp::get_logger("rclcpp"),
+                        "Serial Fail: tcgetattr error (%s)", std::strerror(errno));
+        ::close(fd);
+        return -1;
+    }
+
+    cfsetispeed(&tty, B1000000);
+    cfsetospeed(&tty, B1000000);
+    cfmakeraw(&tty);
+
+    tty.c_cflag &= ~PARENB;
+    tty.c_cflag &= ~CSTOPB;
+    tty.c_cflag &= ~CRTSCTS;
+    tty.c_cflag |= (CS8 | CLOCAL | CREAD);
+
+    tty.c_cc[VMIN]  = 0;
+    tty.c_cc[VTIME] = 0;
+
+    if (tcsetattr(fd, TCSANOW, &tty) != 0) {
+        RCLCPP_ERROR(rclcpp::get_logger("rclcpp"),
+                        "Serial Fail: tcsetattr error (%s)", std::strerror(errno));
+        ::close(fd);
+        return -1;
+    }
+
+    tcflush(fd, TCIOFLUSH);
+
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"),
+                "Serial opened: %s @ 1000000, 8N1, raw", device_name);
+    return fd;
+}
+
+koma::FeetechJointManager::FeetechJointManager(const rclcpp::NodeOptions & options) :
+    Node("joint_manager", options),
+    serial_port(
+      this->declare_parameter<std::string>("port", "platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.1.1:1.0").c_str()
+    )
+{
+    cur_joint_state_pub_ = this->create_publisher<sensor_msgs::msg::JointState> (
+        "joint_states", rclcpp::SensorDataQoS()
+    );
+}
+
+void koma::FeetechJointManager::publish_cur_joint_state() {
+
+}
+
+int main(int argc, char ** argv)
+{
+  rclcpp::init(argc, argv);
+
+  std::shared_ptr<koma::FeetechJointManager> node = std::make_shared<koma::FeetechJointManager>();
+  rclcpp::spin(node);
+
+  rclcpp::shutdown();
+  return 0;
+}
