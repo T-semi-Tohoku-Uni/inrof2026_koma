32472eadde453b45f7496c45edeafd4f674ef2a6
shibatakeigo <sagankeigo17@gmail.com>
2026-05-21 17:42:19 +0900

    fix(feetechservo): serial path

--- src/feetech_servo/src/sts3125.cpp
+++ src/feetech_servo/src/sts3125.cpp
@@ -54,5 +54,5 @@ int koma::FeetechSerial::open_serial(const char* device_name) {
 
 int main(int argc, char ** argv)
 {
-    koma::FeetechSerial serial = koma::FeetechSerial("platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.1.1:1.0");
+    koma::FeetechSerial serial = koma::FeetechSerial("/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.1.1:1.0");
 }
