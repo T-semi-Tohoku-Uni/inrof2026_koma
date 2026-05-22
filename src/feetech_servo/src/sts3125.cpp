471d9fce67ab03572a04f8ecaf891d1c6e5002e4
shibatakeigo <sagankeigo17@gmail.com>
2026-05-22 21:36:40 +0900

    fix(feetech): main func

--- src/feetech_servo/src/sts3125.cpp
+++ src/feetech_servo/src/sts3125.cpp
@@ -676,7 +676,9 @@ int main(int argc, char ** argv)
 
         koma::FeetechState state;
         if(!serial.send_read_state_command()) {}
-        while(!serial.try_read_state_response(state)) {}
+        while(!serial.try_read_state_response(state)) {
+            state.print();
+        }
     }
 
     return 0;
