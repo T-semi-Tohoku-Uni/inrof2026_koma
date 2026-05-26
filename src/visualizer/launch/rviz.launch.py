6107efd3602d7c57115382736a165ac3dceb42aa
shibatakeigo <sagankeigo17@gmail.com>
2026-05-26 13:55:51 +0900

    fix(launch): delete package

--- src/visualizer/launch/rviz.launch.py
+++ src/visualizer/launch/rviz.launch.py
@@ -13,11 +13,9 @@ import math
 
 def generate_launch_description():
     inrof2026_koma_package_dir = get_package_share_directory("inrof2026_koma")
-    simulation_package_dir = get_package_share_directory("simulation")
-    komarm_package_dir = get_package_share_directory("komarm")
-
+    
     # Get workspace dir
-    ws_root = Path(komarm_package_dir).parents[3]
+    ws_root = Path(inrof2026_koma_package_dir).parents[3]
     src_path = str(ws_root / 'src')
     models_path = str(ws_root / 'src' / 'simulation' / 'models')
 
