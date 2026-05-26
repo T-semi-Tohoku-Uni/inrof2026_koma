3ffd9119d4d0f0d377cbbbeeeafd76c19f544e68
shibatakeigo <sagankeigo17@gmail.com>
2026-05-26 14:04:50 +0900

    fix(launch): servo 1

--- src/komarm/launch/komarm_real.launch.py
+++ src/komarm/launch/komarm_real.launch.py
@@ -76,7 +76,6 @@ def generate_launch_description():
 
             #     "servo_2_min": 0.0,
             #     "servo_2_max": math.pi,
-            "is_servo_1_reverse": True,
             "is_servo_2_reverse": True,
             "is_servo_3_reverse": True,
             "is_servo_4_reverse": True,
