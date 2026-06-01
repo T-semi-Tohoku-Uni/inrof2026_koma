5e12715229120591ea57cec0c871e1e86b3ea789
shibatakeigo <sagankeigo17@gmail.com>
2026-06-01 11:06:12 +0900

    refactor(ballpath): format

--- src/planning/include/planning/ball_plan.hpp
+++ src/planning/include/planning/ball_plan.hpp
@@ -1,33 +1,35 @@
 #pragma once
-#include <rclcpp/rclcpp.hpp>
+#include <tf2/LinearMath/Quaternion.h>
+
 #include <geometry_msgs/msg/pose2_d.hpp>
-#include <inrof2026_koma_type/srv/pose_stamped.hpp>
-#include <nav_msgs/msg/path.hpp>
 #include <geometry_msgs/msg/pose_stamped.hpp>
 #include <geometry_msgs/msg/twist.hpp>
-#include <tf2/LinearMath/Quaternion.h>
+#include <inrof2026_koma_type/srv/pose_stamped.hpp>
+#include <nav_msgs/msg/path.hpp>
+#include <rclcpp/rclcpp.hpp>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 #include <visualization_msgs/msg/marker.hpp>
 
-namespace koma{
-    class BallPathNode:public rclcpp::Node{
-        public:
-            BallPathNode(const rclcpp::NodeOptions & options = rclcpp::NodeOptions());
+namespace koma
+{
+class BallPathNode : public rclcpp::Node
+{
+public:
+  BallPathNode(const rclcpp::NodeOptions & options = rclcpp::NodeOptions());
 
-        private:
-            rclcpp::Subscription<geometry_msgs::msg::Pose>::SharedPtr subPose_;
-            rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr path_pub_;
-            void poseCallback(const geometry_msgs::msg::Pose::SharedPtr msg);
-            geometry_msgs::msg::Pose::SharedPtr robot_pose_;
-            void genBallPath(
-                const std::shared_ptr<inrof2026_koma_type::srv::PoseStamped::Request> request,
-                const std::shared_ptr<inrof2026_koma_type::srv::PoseStamped::Response> response
-            );
+private:
+  rclcpp::Subscription<geometry_msgs::msg::Pose>::SharedPtr subPose_;
+  rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr path_pub_;
+  void poseCallback(const geometry_msgs::msg::Pose::SharedPtr msg);
+  geometry_msgs::msg::Pose::SharedPtr robot_pose_;
+  void genBallPath(
+    const std::shared_ptr<inrof2026_koma_type::srv::PoseStamped::Request> request,
+    const std::shared_ptr<inrof2026_koma_type::srv::PoseStamped::Response> response);
 
-            int num_points_;
-            double shorten_;
-            double theta_offset_;
-            rclcpp::Service<inrof2026_koma_type::srv::PoseStamped>::SharedPtr srv_gen_route_;
-            rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr pose_arrow_pub_;
-    };
-}
+  int num_points_;
+  double shorten_;
+  double theta_offset_;
+  rclcpp::Service<inrof2026_koma_type::srv::PoseStamped>::SharedPtr srv_gen_route_;
+  rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr pose_arrow_pub_;
+};
+}  // namespace koma
--- src/planning/src/ball_plan.cpp
+++ src/planning/src/ball_plan.cpp
@@ -1,136 +1,136 @@
 #include <planning/ball_plan.hpp>
 
-namespace koma{
-    BallPathNode::BallPathNode(const rclcpp::NodeOptions & options): Node("ball_path_node", options){
-        subPose_ = create_subscription<geometry_msgs::msg::Pose>(
-            "pose", 10, std::bind(&BallPathNode::poseCallback, this, std::placeholders::_1)
-        );     
+namespace koma
+{
+BallPathNode::BallPathNode(const rclcpp::NodeOptions & options) : Node("ball_path_node", options)
+{
+  subPose_ = create_subscription<geometry_msgs::msg::Pose>(
+    "pose", 10, std::bind(&BallPathNode::poseCallback, this, std::placeholders::_1));
 
-        rclcpp::QoS pathQos = rclcpp::QoS(rclcpp::KeepLast(10))
-                                .reliable()
-                                .transient_local();
-        path_pub_ = create_publisher<nav_msgs::msg::Path>("path", pathQos);
+  rclcpp::QoS pathQos = rclcpp::QoS(rclcpp::KeepLast(10)).reliable().transient_local();
+  path_pub_ = create_publisher<nav_msgs::msg::Path>("path", pathQos);
 
+  srv_gen_route_ = this->create_service<inrof2026_koma_type::srv::PoseStamped>(
+    "generate_ball_path",
+    std::bind(&BallPathNode::genBallPath, this, std::placeholders::_1, std::placeholders::_2));
 
-        srv_gen_route_ = this->create_service<inrof2026_koma_type::srv::PoseStamped>(
-            "generate_ball_path", std::bind(&BallPathNode::genBallPath, this, std::placeholders::_1, std::placeholders::_2)
-        );
+  rclcpp::QoS poseArrowQos(rclcpp::KeepLast(10));
+  pose_arrow_pub_ =
+    this->create_publisher<visualization_msgs::msg::Marker>("pose_arrow_marker", poseArrowQos);
 
-        rclcpp::QoS poseArrowQos(rclcpp::KeepLast(10));
-        pose_arrow_pub_= this->create_publisher<visualization_msgs::msg::Marker>("pose_arrow_marker", poseArrowQos);
+  this->declare_parameter<int>("num_points_", 10);
+  this->declare_parameter<double>("shorten", 0.04);
+  this->declare_parameter<double>("theta_offset", 0.0);
+  this->get_parameter("num_points_", num_points_);
+  this->get_parameter("shorten", shorten_);
+  this->get_parameter("theta_offset", theta_offset_);
+};
 
-        this->declare_parameter<int>("num_points_", 10);
-        this->declare_parameter<double>("shorten", 0.04);
-        this->declare_parameter<double>("theta_offset", 0.0);
-        this->get_parameter("num_points_", num_points_);
-        this->get_parameter("shorten", shorten_);
-        this->get_parameter("theta_offset", theta_offset_);
-    };
+void BallPathNode::poseCallback(const geometry_msgs::msg::Pose::SharedPtr msg)
+{
+  robot_pose_ = msg;
+};
 
-    void BallPathNode::poseCallback(const geometry_msgs::msg::Pose::SharedPtr msg){
-        robot_pose_ = msg;
-    };
+void BallPathNode::genBallPath(
+  const std::shared_ptr<inrof2026_koma_type::srv::PoseStamped::Request> request,
+  const std::shared_ptr<inrof2026_koma_type::srv::PoseStamped::Response> response)
+{
+  RCLCPP_INFO(
+    this->get_logger(), "Generating ball path to (%.2f, %.2f)",
+    request->pose_stamped.pose.position.x, request->pose_stamped.pose.position.y);
+  if (!robot_pose_) {
+    RCLCPP_INFO(this->get_logger(), "no pose");
+    return;
+  }
 
-    
+  //generate path
+  nav_msgs::msg::Path path_msg;
+  path_msg.header.frame_id = "map";
+  path_msg.header.stamp = this->now();
 
-    void BallPathNode::genBallPath(
-        const std::shared_ptr<inrof2026_koma_type::srv::PoseStamped::Request> request,
-        const std::shared_ptr<inrof2026_koma_type::srv::PoseStamped::Response> response
-    ){
-        RCLCPP_INFO(this->get_logger(), "Generating ball path to (%.2f, %.2f)", request->pose_stamped.pose.position.x, request->pose_stamped.pose.position.y);
-        if (!robot_pose_){
-            RCLCPP_INFO(this->get_logger(), "no pose");
-            return;
-        }
+  geometry_msgs::msg::PoseStamped start_pose;
+  geometry_msgs::msg::PoseStamped goal_pose;
+  start_pose.header = path_msg.header;
+  goal_pose.header = path_msg.header;
 
-        //generate path
-        nav_msgs::msg::Path path_msg;
-        path_msg.header.frame_id = "map";
-        path_msg.header.stamp = this->now();
+  //get position
+  start_pose.pose.position.x = robot_pose_->position.x;
+  start_pose.pose.position.y = robot_pose_->position.y;
+  goal_pose.pose.position.x = request->pose_stamped.pose.position.x;
+  goal_pose.pose.position.y = request->pose_stamped.pose.position.y;
 
-        
-        geometry_msgs::msg::PoseStamped start_pose;
-        geometry_msgs::msg::PoseStamped goal_pose;
-        start_pose.header = path_msg.header;
-        goal_pose.header = path_msg.header;
+  double theta = 0.0;
 
-        //get position
-        start_pose.pose.position.x = robot_pose_->position.x;
-        start_pose.pose.position.y = robot_pose_->position.y;
-        goal_pose.pose.position.x = request->pose_stamped.pose.position.x;
-        goal_pose.pose.position.y = request->pose_stamped.pose.position.y;
+  double dx = goal_pose.pose.position.x - start_pose.pose.position.x;
+  double dy = goal_pose.pose.position.y - start_pose.pose.position.y;
+  double distance = sqrt(dx * dx + dy * dy);
+  double ux = dx / distance;
+  double uy = dy / distance;
 
-        double theta = 0.0;
+  if (dx == 0.0 && dy == 0.0) {
+    RCLCPP_INFO(this->get_logger(), "start and goal are the same");
+    return;
+  } else if (dx == 0.0) {
+    if (dy > 0.0) {
+      theta = M_PI / 2.0;
+    } else {
+      theta = -M_PI / 2.0;
+    }
+    return;
+  } else {
+    theta = atan2(dy, dx);
+  }
 
-        double dx = goal_pose.pose.position.x - start_pose.pose.position.x;
-        double dy = goal_pose.pose.position.y - start_pose.pose.position.y;
-        double distance = sqrt(dx*dx + dy*dy);
-        double ux = dx / distance;
-        double uy = dy / distance;
+  //shorten path
+  if (distance > shorten_) {
+    goal_pose.pose.position.x -= shorten_ * ux;
+    goal_pose.pose.position.y -= shorten_ * uy;
+  }
+  //create path
+  for (int i = 0; i <= num_points_; ++i) {
+    double t = static_cast<double>(i) / num_points_;
+    geometry_msgs::msg::PoseStamped p;
+    p.header = path_msg.header;
+    p.pose.position.x =
+      start_pose.pose.position.x + t * (goal_pose.pose.position.x - start_pose.pose.position.x);
+    p.pose.position.y =
+      start_pose.pose.position.y + t * (goal_pose.pose.position.y - start_pose.pose.position.y);
 
-        if (dx == 0.0 && dy == 0.0){
-            RCLCPP_INFO(this->get_logger(), "start and goal are the same");
-            return;
-        }
-        else if (dx == 0.0){
-            if (dy > 0.0){
-                theta = M_PI / 2.0;
-            }else{
-                theta = -M_PI / 2.0;
-            }
-            return;
-        }
-        else{
-            theta = atan2(dy, dx);
-        }
+    tf2::Quaternion q;
+    q.setRPY(0, 0, theta);
+    p.pose.orientation = tf2::toMsg(q);
 
-        //shorten path
-        if (distance > shorten_){
-            goal_pose.pose.position.x -= shorten_ * ux;
-            goal_pose.pose.position.y -= shorten_ * uy;
-	}
-        //create path
-        for (int i=0; i<=num_points_; ++i){
-            double t = static_cast<double>(i) / num_points_;
-            geometry_msgs::msg::PoseStamped p;
-            p.header = path_msg.header;
-            p.pose.position.x = start_pose.pose.position.x + t * (goal_pose.pose.position.x - start_pose.pose.position.x);
-            p.pose.position.y = start_pose.pose.position.y + t * (goal_pose.pose.position.y - start_pose.pose.position.y);
+    path_msg.poses.push_back(p);
+  }
 
-            tf2::Quaternion q;
-            q.setRPY(0, 0, theta);
-            p.pose.orientation = tf2::toMsg(q);
+  visualization_msgs::msg::Marker arrow;
+  geometry_msgs::msg::Pose goal_robot_pose;
 
-            path_msg.poses.push_back(p);
-        }
+  // pub waypoint pose
+  arrow.header.frame_id = "map";
+  arrow.ns = "goal_point_arrow";
+  arrow.id = 0;
+  arrow.type = visualization_msgs::msg::Marker::ARROW;
+  arrow.action = visualization_msgs::msg::Marker::ADD;
+  arrow.pose = path_msg.poses[path_msg.poses.size() - 1].pose;
+  arrow.scale.x = 0.08;
+  arrow.scale.y = 0.04;
+  arrow.scale.z = 0.04;
+  arrow.color.r = 0.0f;
+  arrow.color.g = 0.0f;
+  arrow.color.b = 1.0f;
+  arrow.color.a = 1.0f;
+  pose_arrow_pub_->publish(arrow);
 
-        visualization_msgs::msg::Marker arrow;
-        geometry_msgs::msg::Pose goal_robot_pose;
+  path_pub_->publish(path_msg);
+};
 
-        // pub waypoint pose
-        arrow.header.frame_id = "map";
-        arrow.ns = "goal_point_arrow";
-        arrow.id = 0;
-        arrow.type = visualization_msgs::msg::Marker::ARROW;
-        arrow.action = visualization_msgs::msg::Marker::ADD;
-        arrow.pose = path_msg.poses[path_msg.poses.size()-1].pose;
-        arrow.scale.x = 0.08;
-        arrow.scale.y = 0.04;
-        arrow.scale.z = 0.04;
-        arrow.color.r = 0.0f;
-        arrow.color.g = 0.0f;
-        arrow.color.b = 1.0f;
-        arrow.color.a = 1.0f;
-        pose_arrow_pub_ -> publish(arrow);
+}  // namespace koma
 
-        path_pub_->publish(path_msg);
-    };
-
-}
-
-int main(int argc, char ** argv){
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<yasarobo2025_26::BallPathNode>());
-    rclcpp::shutdown();
-    return 0;
+int main(int argc, char ** argv)
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<yasarobo2025_26::BallPathNode>());
+  rclcpp::shutdown();
+  return 0;
 }
