520f427ae8ce50a06f76414ed9f1e406c5aaf70c
shibatakeigo <sagankeigo17@gmail.com>
2026-06-02 18:41:41 +0900

    feat(launch): add joint manager

--- src/inrof2026_koma/launch/real.launch.py
+++ src/inrof2026_koma/launch/real.launch.py
@@ -7,6 +7,7 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 import launch_ros
+from pathlib import Path
 
 import xacro
 import math
@@ -21,6 +22,9 @@ def generate_launch_description():
     # get each package dir
     inrof2026_koma_package_dir = get_package_share_directory("inrof2026_koma")
     simulation_package_dir = get_package_share_directory("simulation")
+    komarm_package_dir = get_package_share_directory("komarm")
+
+    weight_path = str(Path(komarm_package_dir) / "weight" / "policy.pt")
 
     # get file path
     world_file_path = os.path.join(
@@ -111,13 +115,56 @@ def generate_launch_description():
         output="screen",
     )
 
+    joint_manager = Node(
+        package="komarm",
+        executable="feetech_joint",
+        output="screen",
+        parameters=[
+            {
+            #     "servo_1_min": -math.pi/2,
+            #     "servo_1_max":  math.pi/2,
+
+            #     "servo_2_min": 0.0,
+            #     "servo_2_max": math.pi,
+            "is_servo_2_reverse": True,
+            "is_servo_3_reverse": True,
+            "is_servo_4_reverse": True,
+            # "is_servo_4_reverse": True,
+
+            #     "servo_3_min": 0.0,
+            #     "servo_3_max": math.pi,
+
+            #     "servo_4_min": -math.pi/4.0,
+            #     "servo_4_max": math.pi/2.0,
+
+            #     "servo_5_min": -math.pi,
+            #     "servo_5_max": math.pi,
+            }
+        ]
+    )
+
+    catch_influence = Node(
+        package="komarm",
+        executable="catch_influence",
+        output="screen",
+        parameters=[
+            {
+                "model_path": weight_path
+            }
+        ],
+        remappings=[('clock', '/world/inrof/clock')]
+    )
+
+
     return LaunchDescription([
         launch_ros.actions.SetParameter(name='use_sim_time', value=True),
         node_robot_state_publisher,
         map_server_cmd,
         start_lifecycle_manager_cmd,
         static_from_map_to_odom,
-        uart_bridge,
-        joy2vel,
-        joy_node
+        catch_influence,
+        joint_manager,
+        # uart_bridge,
+        # joy2vel,
+        # joy_node
     ])
