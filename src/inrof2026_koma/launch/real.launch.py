a7cd079f191ea8eacd50dc596603662985a4f258
shibatakeigo <sagankeigo17@gmail.com>
2026-06-07 11:52:43 +0900

    feat(path): update ball path params

--- src/inrof2026_koma/launch/real.launch.py
+++ src/inrof2026_koma/launch/real.launch.py
@@ -329,7 +329,7 @@ def generate_launch_description():
         executable="ball_plan",
         output="screen",
         parameters=[{
-            "shorten": 0.28,
+            "shorten": 0.26,
         }],
     )
 
