cb80990e3068ae7871e51ad6cde25d4091a52b6f
shibatakeigo <sagankeigo17@gmail.com>
2026-06-12 20:13:34 +0900

    feat(color): add debug

--- src/uart_bridge/src/color.cpp
+++ src/uart_bridge/src/color.cpp
@@ -101,6 +101,8 @@ void koma::Color::receive_color_from_serial()
         std_msgs::msg::UInt8 msg_srv;
         msg_srv.data = (std::max(data_byte_1, data_byte_2) - 1) % 3;
         color_response_.color = msg_srv.data;
+
+        RCLCPP_INFO(this->get_logger(), "Received color data: %d, %d -> color: %d", data_byte_1, data_byte_2, msg_srv.data);
       }
 
       recev_buffer_.erase(recev_buffer_.begin(), it_delim + 2);
